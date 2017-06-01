83ef900f3ed2ad425854e9578af79e217c9d2aca
jonathanandrewsuk <jonathanandrewsuk@gmail.com>
2017-05-31 20:19:38 -0400

    materialize

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 
 function searchGiphy(){
   $("form#giphy-search").on("submit", function(event){
-    var num = 0 
+    var num = 0
     var objectArray = []
     event.preventDefault()
     var searching = $("#searching").val().split(' ')
@@ -14,47 +14,74 @@ function searchGiphy(){
         return `http://api.giphy.com/v1/gifs/search?q=${element}&api_key=dc6zaTOxFJmzC`
       })
 
-  
+
       searchArray.forEach(function(element){
         $.ajax({
           url: element,
           async: false,
-          success: function(data){ 
+          success: function(data){
           objectArray.push(data.data)
          }})
       })
       renderGiphy(objectArray)
-    }) 
+    })
   }
 
 
 function renderGiphy(objectArray){
 console.log(objectArray)
-var num0 = 0 
-var num1 = 0 
-var num2 = 0 
-  
-  $(`#images0`).html(`<div style="width:100%;height:0;padding-bottom:65%;position:relative;"><iframe src="https://giphy.com/embed/${objectArray[0][num0].id}" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div>`)
+var num0 = 0
+var num1 = 0
+var num2 = 0
 
-  $(`#images1`).html(`<div style="width:100%;height:0;padding-bottom:65%;position:relative;"><iframe src="https://giphy.com/embed/${objectArray[1][num1].id}" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div>`)
+  $(`#images0`).html(`<div>
+    <div class="card">
+      <div class="card-image" id="card-image0">
+        <div style="width:100%;height:0;padding-bottom:100%;position:relative;"><iframe src="https://giphy.com/embed/${objectArray[0][num0].id}" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div>
+      </div>
+      <div class="card-action">
+        <a class="card-action0" href="#">Shuffle</a>
+      </div>
+    </div>
+  </div>`)
 
-  $(`#images2`).html(`<div style="width:100%;height:0;padding-bottom:65%;position:relative;"><iframe src="https://giphy.com/embed/${objectArray[2][num2].id}" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div>`)
+  $(`#images1`).html(`<div>
+    <div class="card">
+      <div class="card-image" id="card-image1">
+        <div style="width:100%;height:0;padding-bottom:100%;position:relative;"><iframe src="https://giphy.com/embed/${objectArray[1][num0].id}" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div>
+      </div>
+      <div class="card-action">
+        <a class="card-action1" href="#">Shuffle</a>
+      </div>
+    </div>
+  </div>`)
+
+  $(`#images2`).html(`<div>
+    <div class="card">
+      <div class="card-image" id="card-image2">
+        <div style="width:100%;height:0;padding-bottom:100%;position:relative;"><iframe src="https://giphy.com/embed/${objectArray[2][num0].id}" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div>
+      </div>
+      <div class="card-action">
+        <a class="card-action2" href="#">Shuffle</a>
+      </div>
+    </div>
+  </div>`)
 
 
-  $("input#shuffle0").on('click',(function(event){
+  $(".card-action0").on('click',(function(event){
     num0++
-    $(`#images0`).html(`<div style="width:100%;height:0;padding-bottom:65%;position:relative;"><iframe src="https://giphy.com/embed/${objectArray[0][num0].id}" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div>`)
+    $(`#card-image0`).html(`<div style="width:100%;height:0;padding-bottom:100%;position:relative;"><iframe src="https://giphy.com/embed/${objectArray[0][num0].id}" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div>`)
   }))
 
-  $("input#shuffle1").on('click',(function(event){
+  $(".card-action1").on('click',(function(event){
     num1++
-    $(`#images1`).html(`<div style="width:100%;height:0;padding-bottom:65%;position:relative;"><iframe src="https://giphy.com/embed/${objectArray[1][num1].id}" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div>`)
+    $(`#card-image1`).html(`<div style="width:100%;height:0;padding-bottom:100%;position:relative;"><iframe src="https://giphy.com/embed/${objectArray[1][num1].id}" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div>`)
   }))
 
-  $("input#shuffle2").on('click',(function(event){
+  $(".card-action2").on('click',(function(event){
     num2++
-    $(`#images2`).html(`<div style="width:100%;height:0;padding-bottom:65%;position:relative;"><iframe src="https://giphy.com/embed/${objectArray[2][num2].id}" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed"  allowFullScreen></iframe></div>`)
-  })) 
+    $(`#card-image2`).html(`<div style="width:100%;height:0;padding-bottom:100%;position:relative;"><iframe src="https://giphy.com/embed/${objectArray[2][num2].id}" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed"  allowFullScreen></iframe></div>`)
+  }))
 }
 
 
@@ -75,4 +102,3 @@ $( "#guess" ).submit(function(event){
     alert("You were rong")
   }
 });
-
